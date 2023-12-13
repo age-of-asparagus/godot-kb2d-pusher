682ab957e9fbe7494ab032bbc1dd27ad3fd2154b
Tylere Couture <tylerecouture@gmail.com>
2023-12-13 08:36:06 -0800

    changes

--- Enemy.gd
+++ Enemy.gd
@@ -3,7 +3,7 @@ onready var player = $"../Player"
 var velocity := Vector2.ZERO
 export var strength = 100
 var speed = 50
-var pushed = false
+var being_pushed = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,15 +11,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-#	print(pushed)
 	
 	var direction : Vector2 = (player.global_position-global_position).normalized()
 	
-	if not pushed:
+	if not being_pushed:
 		velocity = move_and_slide(direction*speed)
 	else:
-		pushed = false
-	
-	
-	
+		being_pushed = false
 	
--- Player.gd
+++ Player.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
+
+onready var line = $Line2D
+
 var velocity := Vector2.ZERO
 export var speed = 300
 export var strength = 200
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -20,12 +23,12 @@ func _physics_process(delta):
 		if collider is KinematicBody2D:
 			# Dot product will determine movement direction aligning with the collision direction:
 			var aligned_push = -direction.dot(collision.normal)
-#			print(aligned_push)
 			
 			var net_push = aligned_push*strength - collider.strength
 			net_push = max(net_push, 0)  # if net push negative, treat as zero
-			print("Push: ", net_push)
+#			print("Push: ", net_push)
 			var push_velocity = -collision.normal*net_push
+			
 			collider.move_and_slide(push_velocity)
-			collider.pushed = true
-		
+			collider.being_pushed = true
+			
